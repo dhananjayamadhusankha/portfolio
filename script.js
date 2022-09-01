49fb7c11cc3098d755bce646c3b3dfd00b997018
JanithDilsh <it20148454@my.sliit.lk>
2022-09-01 21:41:44 +0530

    js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["an Undergraduate", "a Developer", "an Designer"],
+        strings: ["an Undergraduate", "a Developer", "a Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
